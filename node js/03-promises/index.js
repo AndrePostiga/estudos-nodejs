31ecf89ca831b31c453e335f134fc317aca03a39
Andre Postiga <andre.postiga@msn.com>
2020-03-10 07:52:52 -0300

    Entendendo Promisses

--- node js/03-promises/index.js
+++ node js/03-promises/index.js
@@ -1,3 +1,9 @@
+//importar um modulo interno para resolver promisses
+const util = require('util')
+
+const obterEnderecoAsync = util.promisify(obterEndereco)
+
+
 function obterUsuario(){
     //Quando der algum problema -> Reject(erro)
     //Quando for sucesso -> resolve
@@ -38,22 +44,38 @@ function obterEndereco(idUsuario){
     })
 }
 
-const usuarioPromisse = obterUsuario()
-usuarioPromisse
-    .then( resultado => {
-        return obterTelefone(resultado.id)
-        .then( result => {
-            return 
+function obterEndereco(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null, {
+            rua: 'Amadeu Amaral',
+            numero: '50',
+            quadra: 'B',
+            bairro: 'Arsenal'
         })
-    })  
-    .then(resultado => {
-        return Object.assign(resultado, obterEndereco(resultado.id))
-    })  
-    .then( resultado => {
-        console.log(resultado)
-    })
-    .catch( (erro) => {
-        console.error('Deu ruim', erro)
-    })
+    }, 3000);
+}
+
+
 //para manipular o sucesso usamos a função .then, já que retorna promisse
 //para manipular erros usamos o .catch
+//usuario -> telefone -> consola telefone
+const usuarioPromisse = obterUsuario()
+usuarioPromisse
+.then( resultado => {
+    return obterTelefone(resultado.id)
+    .then( function resolverTelefone(result) {
+        return Object.assign(resultado, {contato : result })
+    })
+})
+.then( function resultadoAnterior (resultado) {
+    return obterEnderecoAsync(resultado.id)
+    .then( function resolver(result) {
+        return Object.assign(resultado, {endereco : result})
+    })
+})  
+.then( resultado => {
+    console.log(resultado)
+})
+.catch( (erro) => {
+    console.error('Deu ruim', erro)
+})
